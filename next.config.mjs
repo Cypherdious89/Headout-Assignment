164b47d829f365bad9f1c07fcef5fba34bee7020
Priyansh Singh <priyanshsingh1@gmail.com>
2025-03-02 03:51:49 +0530

    Revert "Adding gh-pages"
    This reverts commit 729c2d965dd21a67429f73e4f865f2c88693f7a5.

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  basePath: process.env.NODE_ENV === "production" ? "/Headout-Assignment" : "",
-  assetPrefix: process.env.NODE_ENV === "production" ? "/Headout-Assignment/" : "",
-  images: {
-    unoptimized: true,
-  },
 };
 
 export default nextConfig;
