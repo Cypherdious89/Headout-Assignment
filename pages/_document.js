83ff534537f63aae5a05ddaa35eba8ace1328724
Priyansh Singh <priyanshsingh1@gmail.com>
2025-03-02 02:01:32 +0530

    Changed font family

--- pages/_document.js
+++ pages/_document.js
@@ -3,6 +3,10 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
+      <link
+        href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;500;700&display=swap"
+        rel="stylesheet"
+      />
       <Head />
       <body className="antialiased">
         <Main />
